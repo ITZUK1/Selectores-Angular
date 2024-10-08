70c66cd4ac3277b1b30c8f5d84574ebd15ad55f1
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-10-08 11:51:57 -0500

    se creo el form y el ruteo de el selector

--- selector/src/app/app-routing.module.ts
+++ selector/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{
-  path:'**',
-  redirectTo: 'selector',
-  loadChildren:()=>import('../app/countries/countries.module').then (m=>m.CountriesModule),
-}];
+const routes: Routes = [
+
+  {
+    path: 'selector',
+    loadChildren: () => import('./countries/countries.module').then( m => m.CountriesModule ),
+  },
+  {
+    path: '**',
+    redirectTo: 'selector',
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
