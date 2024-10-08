78e08944b6c8173b97f6e1b132799d79b40ec088
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-10-08 13:40:04 -0500

    se agrego el otro campo de continente

--- selector/src/app/countries/pages/selector-page/selector-page.component.ts
+++ selector/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { filter, switchMap, tap } from 'rxjs';
+
+import { CountriesService } from '../../services/countries.service';
+import { Region, SmallCountry } from '../../interfaces/country.interfaces';
 
 @Component({
   selector: 'app-selector-page',
@@ -7,12 +11,34 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styles: [
   ]
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit {
 
-public myForm:FormGroup =this.fb.group({
-  region:['',Validators.required],
-  country:['',Validators.required],
-  borders:['',Validators.required],
-})
-constructor(private fb:FormBuilder,){}
+  public countriesByRegion: SmallCountry[] = [];
+  public borders: SmallCountry[] = [];
+
+  public myForm: FormGroup = this.fb.group({
+    region : ['', Validators.required ],
+    country: ['', Validators.required ],
+    border : ['', Validators.required ],
+  });
+
+
+  constructor(
+    private fb: FormBuilder,
+    private countriesService: CountriesService,
+  ) {}
+
+  ngOnInit(): void {
+    this.onRegionChanged();
+    }
+
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
+
+  onRegionChanged():void{
+    this.myForm.get('region')!.valueChanges
+    .subscribe(regions =>{console.log({regions})})
+  }
 }
